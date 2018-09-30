44a389ee8f4ee7f8d62885789ec1952f0c31778d
Eduardo Domingos <epcdomingos@gmail.com>
2018-10-01 00:00:51 +0100

    Move post thumb to top

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,12 +11,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	<?php amaedoandre_post_thumbnail(); 
+		the_title( '<h1 class="entry-title">', '</h1>' );
 
 		if ( 'post' === get_post_type() ) :
 			?>
@@ -28,8 +24,6 @@
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-
-	<?php amaedoandre_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
