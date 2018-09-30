ec3bef553b460d59002312b486181b29fa8134b5
Eduardo Domingos <epcdomingos@gmail.com>
2018-09-30 15:45:01 +0100

    Set primary menu

--- header.php
+++ header.php
@@ -40,10 +40,9 @@
 	</header><!-- #masthead -->
 
 	<nav id="site-navigation" class="main-navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'amaedoandre' ); ?></button>
 		<?php
 		wp_nav_menu( array(
-			'theme_location' => 'menu-1',
+			'theme_location' => 'primary',
 			'menu_id'        => 'primary-menu',
 		) );
 		?>
