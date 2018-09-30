0cbd9ee0f628708272d78c45af2e5ea6435eaf06
Eduardo Domingos <epcdomingos@gmail.com>
2018-09-30 15:18:02 +0100

    Enqueue google fonts

--- functions.php
+++ functions.php
@@ -103,6 +103,8 @@ add_action( 'widgets_init', 'amaedoandre_widgets_init' );
  * Enqueue scripts and styles.
  */
 function amaedoandre_scripts() {
+	wp_enqueue_style( 'amaedoandre-google-fonts', '//fonts.googleapis.com/css?family=Amatic+SC:400,700|Lora:400,400i,700,700i' );
+
 	wp_enqueue_style( 'amaedoandre-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'amaedoandre-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
