da0a952509db05f42157f46538537888b2439e5a
Eduardo Domingos <epcdomingos@gmail.com>
2018-10-01 17:04:36 +0100

    Add edit post link. Replace the_content by the_excerpt.

--- template-parts/content.php
+++ template-parts/content.php
@@ -22,34 +22,14 @@
 				amaedoandre_posted_on();
 				amaedoandre_comments_link();
 				amaedoandre_posted_by();
+				amaedoandre_edit_post_link();
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'amaedoandre' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'amaedoandre' ),
-			'after'  => '</div>',
-		) );
-		?>
+		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php amaedoandre_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
