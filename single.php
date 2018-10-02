891f3aa4915b4e78e21989274aaf3a775f22db9e
Eduardo Domingos <epcdomingos@gmail.com>
2018-10-02 01:15:35 +0100

    Style post and posts nav

--- single.php
+++ single.php
@@ -18,7 +18,17 @@ get_header();
 
 		get_template_part( 'template-parts/content', 'single' );
 
-		the_post_navigation();
+		// Previous/next post navigation.
+		$next_post = get_next_post();
+		$previous_post = get_previous_post();
+		the_post_navigation( array(
+			'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post:', 'amaedoandre' ) . '</span> ' .
+				'<span class="screen-reader-text">' . __( 'Next post:', 'amaedoandre' ) . '</span> ' .
+				'<span class="post-title">%title</span>' . get_the_post_thumbnail($next_post->ID,'thumbnail', array( 'class' => 'post-thumb' )),
+			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post:', 'amaedoandre' ) . '</span> ' .
+				'<span class="screen-reader-text">' . __( 'Previous post:', 'amaedoandre' ) . '</span> ' .
+				'<span class="post-title">%title</span>' . get_the_post_thumbnail($previous_post->ID,'thumbnail', array( 'class' => 'post-thumb' )),
+		) );
 
 		// If comments are open or we have at least one comment, load up the comment template.
 		if ( comments_open() || get_comments_number() ) :
