e541625dfcbfad46ec97a8514f83448a666e972d
Eduardo Domingos <epcdomingos@gmail.com>
2018-10-12 22:53:25 +0100

    Add space

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<button id="js-to-top-button" class="to-top"><?php esc_html_e( 'Back to top', 'amaedoandre' ); ?> <?php echo amaedoandre_get_svg( array( 'icon' => 'arrow-up' ));?></button>
+		<button id="js-to-top-button" class="to-top"><?php esc_html_e( 'Back to top', 'amaedoandre' ); ?> <?php echo amaedoandre_get_svg( array( 'icon' => 'arrow-up' )); ?></button>
 		<small><?php printf( esc_html__( '&copy; %1$s %2$s. All Rights Reserved.', 'amaedoandre' ), date('Y'), get_bloginfo( 'name' )); ?></small>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
