3d32e90bb346ebb4c14263f411c066b60c1a7153
Eduardo Domingos <epcdomingos@gmail.com>
2018-10-13 02:25:07 +0100

    Fix structure

--- archive.php
+++ archive.php
@@ -10,47 +10,45 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<main id="main" class="main-content">
 
-		<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+		<header class="page-header">
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
 
-			endwhile;
+			/*
+				* Include the Post-Type-specific template for the content.
+				* If you want to override this in a child theme, then include a file
+				* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				*/
+			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_posts_pagination( array(
-				'prev_text' => __( 'Newer', 'amaedoandre' ),
-				'next_text' => __( 'Older', 'amaedoandre' ),
-				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'amaedoandre' ) . '</span>',
-			));
+		endwhile;
 
-		else :
+		the_posts_pagination( array(
+			'prev_text' => __( 'Newer', 'amaedoandre' ),
+			'next_text' => __( 'Older', 'amaedoandre' ),
+			'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'amaedoandre' ) . '</span>',
+		));
 
-			get_template_part( 'template-parts/content', 'none' );
+	else :
 
-		endif;
-		?>
+		get_template_part( 'template-parts/content', 'none' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	endif;
+	?>
+
+	</main><!-- #main -->
 
 <?php
 get_sidebar();
