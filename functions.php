d3175556e4b8f0cc111833e924e5166fbe709f6d
Eduardo Domingos <epcdomingos@gmail.com>
2018-10-15 00:43:28 +0100

    Add image size

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ if ( ! function_exists( 'amaedoandre_setup' ) ) :
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
+		add_image_size( 'gallery', 570, 380, true );
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
